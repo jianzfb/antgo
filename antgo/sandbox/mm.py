b65d00904f0ba12c9ab264d4c3c710a7fc7973ee
zjian <jian.fbehind@gmail.com>
2017-12-14 15:38:31 +0800

    fix readme

---
+++ antgo/sandbox/mm.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# Time: 12/14/17
+# File: mm.py
+# Author: jian<jian@mltalker.com>
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
--- antgo/sandbox/sandbox.py
+++ antgo/sandbox/sandbox.py
@@ -10,7 +10,22 @@ import os, sys
 from contextlib import contextmanager
 from antgo.utils.concurrency import *
 from antgo.utils import logger
+import tarfile
 import time
+import tempfile
+import ipfsapi
+import shutil
+
+def _hierarchy_folder_tar(tar, level):
+  return tar
+
+_ipfs_api_controler = None
+def ipfa_api_controler():
+  global _ipfs_api_controler
+  if _ipfs_api_controler is None:
+    _ipfs_api_controler = ipfsapi.connect('127.0.0.1', 5001)
+
+  return _ipfs_api_controler
 
 @contextmanager
 def running_sandbox(*wargs, **kwargs):
@@ -23,7 +38,35 @@ def running_sandbox(*wargs, **kwargs):
       if now_time - sandbox_start_time > sandbox_running_time:
         logger.info('running time is arriving')
         os._exit(0)
-  
+
+    if 'sandbox_dump_dir' in kwargs and 'token' in kwargs:
+      dump_dir = kwargs['sandbox_dump_dir']
+      try:
+        # 1.step tar dump dir (tempdir)
+        tar_file_path = os.path.join(tempfile.tempdir, '%s.tar'%dump_dir.split('/')[-1])
+        tar = tarfile.open(tar_file_path, 'w')
+        tar = _hierarchy_folder_tar(tar, dump_dir)
+        tar.close()
+
+        # 2.step symmetric encryption
+        encryption_tar_file_path = tar_file_path
+
+        # 3.step add to ipfs (kv: experiment <-> HASH)
+        # 3.1.step delete old record
+        old_dump_hash = None
+
+        # 3.2.step add new record
+        cc = ipfa_api_controler()
+        res = cc.add(tar_file_path)
+        new_dump_hash = res['Hash']
+
+        # 4.step clear data
+        shutil.rmtree(tar_file_path)
+        shutil.rmtree(encryption_tar_file_path)
+      except:
+        pass
+
+
   # timer thread
   timer_thread = None
 
