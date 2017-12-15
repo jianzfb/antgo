b555188694087e64526296c552d55055f698d129
jian.fbehind@gmail.com <jian.fbehind@gmail.com>
2017-12-15 16:57:27 +0800

    add codebook

---
+++ antgo/codebook/tf/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+# @Time    : 17-12-14
+# @File    : __init__.py.py
+# @Author  : jian<jian@mltalker.com>
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
