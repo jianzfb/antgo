3aa626723d7efdfbaa3e1d03fea5985463ca7f72
jian <jian.fbehind@gmail.com>
2018-03-24 17:55:34 +0800

    reorganize structure

--- antgo/example/siamese_fc.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-# @Time    : 18-1-11
-# @File    : siamese_fc.py
-# @Author  : 
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
--- antgo/example/tacotron.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-# @Time    : 18-1-2
-# @File    : tacotron.py
-# @Author  : 
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-
