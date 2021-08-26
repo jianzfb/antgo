2bd7d3999d89d88fc00db4fbe9b884bc748e31da
zhangjian52 <zhangjian52@baidu.com>
2021-08-26 12:05:39 +0000

    u

--- antgo/dataflow/imgaug/__init__.py
+++ antgo/dataflow/imgaug/__init__.py
@@ -2,21 +2,3 @@
 # @Time    : 17-6-22
 # @File    : __init__.py
 # @Author  : jian<jian@mltalker.com>
-import os
-from pkgutil import walk_packages
-
-__all__ = []
-
-
-def global_import(name):
-  p = __import__(name, globals(), locals(), level=1)
-  lst = p.__all__ if '__all__' in dir(p) else dir(p)
-  del globals()[name]
-  for k in lst:
-    globals()[k] = p.__dict__[k]
-
-for _, module_name, _ in walk_packages(
-        [os.path.dirname(__file__)]):
-  if not module_name.startswith('_'):
-    global_import(module_name)
-
