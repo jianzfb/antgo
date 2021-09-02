ff92d7dce3043d10fbfda6cef62bf9a7e5a35369
zhangjian52 <zhangjian52@baidu.com>
2021-09-02 15:44:36 +0000

    完善al功能

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def readme():
 
 
 setup(name='antgo',
-      version='0.0.8',
+      version='0.0.9',
       description='machine learning experiment platform',
       __short_description__='machine learning experiment platform',
       url='https://github.com/jianzfb/antgo',
