e9793a8b331d872e35791ab764194fa75d1b9c71
zhangjian.52 <zhangjian.52@bytedance.com>
2023-04-07 09:48:47 +0000

    update version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def read_requirements(filename):
 
 
 setup(name='antgo',
-      version='0.0.20',
+      version='0.0.21',
       description='machine learning experiment platform',
       __short_description__='machine learning experiment platform',
       url='https://github.com/jianzfb/antgo',
