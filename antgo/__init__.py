310703a5475bff616f776054144bbf1ba9357c4b
jian.fbehind <jian.fbehind@gmail.com>
2023-05-06 13:29:25 +0800

    fix release version code

--- antgo/__init__.py
+++ antgo/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from __future__ import division
 
-from .version import version, version_info
+from .version import version_code, version_info
 
-__version__ = version
+__version__ = version_code
 __title__ = 'antgo'
