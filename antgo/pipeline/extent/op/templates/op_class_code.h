1d230eba47389aa582007eaa1a2bdd541ff9eb14
jian.fbehind <jian.fbehind@gmail.com>
2023-08-17 15:26:50 +0000

    support classop/funcop deploy

---
+++ antgo/pipeline/extent/op/templates/op_class_code.h
@@ -0,0 +1,30 @@
+#ifndef _EAGLEEYE_${op_name}_OP_
+#define _EAGLEEYE_${op_name}_OP_
+#include "eagleeye/engine/nano/dataflow/base.h"
+#include "eagleeye/basic/Tensor.h"
+#include <string>
+#include <vector>
+
+class  ${cls_name};
+namespace eagleeye{
+namespace dataflow{
+class ${op_name}:public BaseOp<${input_num}, ${output_num}>{
+public:
+    ${op_name}();
+    virtual ~${op_name}();
+
+    virtual int init(std::map<std::string, std::vector<float>> params);
+    virtual int init(std::map<std::string, std::vector<std::vector<float>>> params){return 0;};
+    virtual int init(std::map<std::string, std::vector<std::string>> params){return 0;};
+    virtual int runOnCpu(const std::vector<Tensor>& input);
+    virtual int runOnGpu(const std::vector<Tensor>& input);
+
+private:
+    ${cls_name}* m_${cls_name};
+};
+
+} // namespace dataflow
+} // namespace eagleeye
+
+
+#endif
