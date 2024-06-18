d10da254235c57417bb5f1b527f7c9e88a9d6aa5
jian.fbehind <jian.fbehind@gmail.com>
2024-06-18 12:51:35 +0000

    fix

--- antgo/script/ssh-launch.sh
+++ antgo/script/ssh-launch.sh
@@ -12,6 +12,6 @@ env=$8
 if [ "$env" = "-" ] ; then
 docker run --rm -d --shm-size="50G" -w /tiger -v /home/${username}/${project}:/tiger -v /data:/dataset --gpus all --ipc=host --net=host --privileged ${image} sh -c "cd /tiger && $command"
 else
-docker run --rm -d --shm-size="50G" -w /tiger -v /home/${username}/${project}:/tiger -v /data:/dataset --gpus all --ipc=host --net=host --privileged ${image} sh -c "cd /tiger && pip3 install --upgrade --force-reinstall git+https://github.com/jianzfb/antgo.git@$env && $command"
+docker run --rm -d --shm-size="50G" -w /tiger -v /home/${username}/${project}:/tiger -v /data:/dataset --gpus all --ipc=host --net=host --privileged ${image} sh -c "cd /tiger && pip3 install --ignore-installed git+https://github.com/jianzfb/antgo.git@$env && $command"
 fi
 # docker run --rm -d --shm-size="50G" -w /tiger -v /home/${username}/${project}:/tiger -v /data:/dataset --gpus all --ipc=host --net=host ${image} sh -c "cd /tiger && pip3 install --upgrade --force-reinstall git+https://github.com/jianzfb/antgo.git@2.2 && $command"
