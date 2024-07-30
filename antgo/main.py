a23e610caea0821d4dc9befc876145f3528714ab
jian.fbehind <jian.fbehind@gmail.com>
2024-07-30 09:12:08 +0000

    support ngrok

--- antgo/main.py
+++ antgo/main.py
@@ -85,6 +85,8 @@ DEFINE_string("tags", None, "tag info")
 DEFINE_string("no-tags", None, "tag info")
 DEFINE_indicator("feedback", True, "")
 DEFINE_indicator("user-input", True, "")
+DEFINE_indicator('ngrok', True, "whether enable network tunnel")
+DEFINE_string("authtoken", "", "3rd platform token")
 DEFINE_int('num', 0, "number")
 DEFINE_indicator("to", True, "")
 DEFINE_indicator("from", True, "")
@@ -198,6 +200,18 @@ def main():
 
     if args.ip == "":
       args.ip = '0.0.0.0'
+    if args.ngrok:
+      if args.authtoken == "":
+        print('Mut set --authtoken')
+        return
+
+      from pyngrok import ngrok
+      public_url = ngrok.connect(args.port).public_url
+      print(f'ngrok public url {public_url}')
+
+      os.system(f'NGROK_AUTHTOKEN={args.authtoken} uvicorn {args.main} --reload --port {args.port} --host {args.ip}')
+      return
+
     os.system(f'uvicorn {args.main} --reload --port {args.port} --host {args.ip}')
     return
 
