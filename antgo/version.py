5750dbd418481b3d52411bc99270e05820e52853
jian.fbehind <jian.fbehind@gmail.com>
2024-10-24 03:26:38 +0000

    add fastapi session depent

--- antgo/version.py
+++ antgo/version.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 from __future__ import division
 
-version_code = "0.3.2"
+version_code = "0.3.3"
 version_info = tuple([int(d) for d in version_code.split("-")[0].split(".")])
