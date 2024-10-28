5fe9f0c7f7b4af35484c212c78eaa7cab9d4ce55
jian.fbehind <jian.fbehind@gmail.com>
2024-10-28 10:51:46 +0000

    fix control for op

--- antgo/pipeline/control/for_op.py
+++ antgo/pipeline/control/for_op.py
@@ -13,7 +13,7 @@ class For(object):
 
     def __call__(self, *args):
         out_list = None
-        loop_num = min([v.shape[0] for v in args])
+        loop_num = max([v.shape[0] for v in args])
         for loop_i in range(loop_num):
             data_tuple = [v[loop_i%v.shape[0]] for v in args]
             out = self.func(*data_tuple)
@@ -22,7 +22,7 @@ class For(object):
 
             if out_list is None:
                 out_list = [[] for _ in range(len(out))]
-            
+
             for elem_i, elem_v in enumerate(out):
                 out_list[elem_i].append(elem_v)
         if out_list is None:
